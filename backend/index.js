7362fb7fa8692b7f24667a0b6a8487cd0a4f909c
ImrazAhmedTanim <tanimf1@gmail.com>
2024-01-30 00:35:36 +0600

    fG

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const routerfuel = require('./controller/router');
  dotenv.config();
 const app = express();
 const corsOptions = {
-  origin: 'https://restapi-70xj.onrender.com', // Frontend origin
+  origin: 'http://localhost:3001', // Frontend origin
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
