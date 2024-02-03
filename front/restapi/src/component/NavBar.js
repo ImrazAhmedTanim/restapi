6946cb15ae913e4dd70986c8d4ae70cf4e3efbf8
ImrazAhmedTanim <tanimf1@gmail.com>
2024-02-04 02:27:00 +0600

    aF

--- front/restapi/src/component/NavBar.js
+++ front/restapi/src/component/NavBar.js
@@ -13,38 +13,34 @@ const NavBar = () => {
   const { isLoggedIn, handleLogout } = useAuth();  // Ensure you're using the correct context values
  
   return (
-<nav  className="nav bg-white shadow-md">
-  <div className="max-w-screen-xl mx-auto px-4">
-    <div className="flex justify-between items-center py-4">
-     
-      
-     
-      {/* Responsive menu items */}
-      <div className="md:flex items-center space-x-4 ml-auto">
-        <div className="text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700">
-          {isLoggedIn ? (
-            <Link to="/" onClick={handleLogout}>Logout</Link>
-          ) : (
-            <Link to="/">Login</Link>
-          )}
-        </div>
-        <div className='text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700'>
-          <Link to="/allusers">Get All Users</Link>
-        </div>
-        <div className='text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700'>
-          <Link to="/fuelpricecalender">Fuel Price</Link>
-        </div>
-        <div className='text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700'>
-          <Link to="/getuser">Get User</Link>
-        </div>
-        <div className='text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700'>
-          <Link to="/updateuser">Update User</Link>
-        </div>
+<nav className="nav bg-white shadow-md">
+  <div className="max-w-screen-xl mx-auto px-4 flex justify-between items-center py-4">
+   
+    
+    <div className="flex items-center space-x-4">
+      <div className="text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700">
+        {isLoggedIn ? (
+          <Link to="/" onClick={handleLogout}>Logout</Link>
+        ) : (
+          <Link to="/">Login</Link>
+        )}
+      </div>
+      <div className='text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700'>
+        <Link to="/allusers">Get All Users</Link>
+      </div>
+      <div className='text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700'>
+        <Link to="/fuelpricecalender">Fuel Price</Link>
+      </div>
+      <div className='text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700'>
+        <Link to="/getuser">Get User</Link>
+      </div>
+      <div className='text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700'>
+        <Link to="/updateuser">Update User</Link>
       </div>
     </div>
   </div>
- 
 </nav>
+
 
 
 
