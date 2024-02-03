91a649232bfcce5e83f69628ef8c1012a3aa8dd1
ImrazAhmedTanim <tanimf1@gmail.com>
2024-02-04 02:13:38 +0600

    layout

--- front/restapi/src/component/NavBar.js
+++ front/restapi/src/component/NavBar.js
@@ -13,36 +13,41 @@ const NavBar = () => {
   const { isLoggedIn, handleLogout } = useAuth();  // Ensure you're using the correct context values
  
   return (
-<nav x-data="{ open: false }" className="nav bg-white shadow-md">
+<nav  className="nav bg-white shadow-md">
   <div className="max-w-screen-xl mx-auto px-4">
     <div className="flex justify-between items-center py-4">
-      <div className="flex items-center">
-        <a href="#" className="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg focus:outline-none focus:shadow-outline">
-          Tanim's Rest Api
-        </a>
+     
+      
+     
+      {/* Responsive menu items */}
+      <div className="md:flex items-center space-x-4 ml-auto">
+        <div className="text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700">
+          {isLoggedIn ? (
+            <Link to="/" onClick={handleLogout}>Logout</Link>
+          ) : (
+            <Link to="/">Login</Link>
+          )}
+        </div>
+        <div className='text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700'>
+          <Link to="/allusers">Get All Users</Link>
+        </div>
+        <div className='text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700'>
+          <Link to="/fuelpricecalender">Fuel Price</Link>
+        </div>
+        <div className='text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700'>
+          <Link to="/getuser">Get User</Link>
+        </div>
+        <div className='text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700'>
+          <Link to="/updateuser">Update User</Link>
+        </div>
       </div>
-      <div className="hidden md:flex items-center space-x-4">
-        <Link to="/" className="text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700">
-          Login
-        </Link>
-        <Link to="/allusers" className="text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700">
-        Get All Users
-        </Link>
-        <Link to="/fuelpricecalender" className="text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700">
-        Fuel Price
-        </Link>
-        <Link to="/getuser" className="text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700">
-        Get User
-        </Link>
-        <Link to="/updateuser" className="text-sm font-semibold text-gray-900 hover:text-blue-700 focus:outline-none focus:text-gray-700">
-        Update User
-        </Link>
-      </div>
-    
     </div>
-  
   </div>
+ 
 </nav>
+
+
+
 
 
 
