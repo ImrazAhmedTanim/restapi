e12509c7da30e9517cd5ae57c5138552086a4096
ImrazAhmedTanim <tanimf1@gmail.com>
2024-02-05 16:05:46 +0600

    sF

--- front/restapi/src/component/Login.js
+++ front/restapi/src/component/Login.js
@@ -50,7 +50,7 @@ setLoading(true);
  <button type="button" className="bg-indigo-400 h-max w-max rounded-lg text-white font-bold hover:bg-indigo-300 hover:cursor-not-allowed duration-[500ms,800ms]" disabled>
      <div className="flex items-center justify-center m-[10px]"> 
          <div className="h-5 w-5 border-t-transparent border-solid animate-spin rounded-full border-white border-4"></div>
-         <div className="ml-2"> Processing... plz wait as the site is hosted in free server</div>
+         <div className="ml-2"> Processing...</div>
      </div>
  </button>
 </div>        ):(
--- front/restapi/src/component/NavBar.js
+++ front/restapi/src/component/NavBar.js
@@ -15,7 +15,7 @@ const NavBar = () => {
   return (
 <nav  className="nav">
   <ul className="navbar flex flex-row justify-end ml-auto">
-    <li className="mb-4 bg-violet-500 border p-2 transition-transform transform hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
+    <li className="mb-4 bg-violet-300 border p-2 transition-transform transform hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
       {isLoggedIn ? (
         <Link to="/" onClick={handleLogout}>Logout</Link>
       ) : (
@@ -23,7 +23,7 @@ const NavBar = () => {
       )}
     </li>
 
-    <li className="mb-4 bg-violet-400 border p-2 transition-transform transform hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
+    <li className="mb-4 bg-violet-300 border p-2 transition-transform transform hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
       <Link to="/allusers">Get All Users</Link>
     </li>
 
@@ -31,11 +31,11 @@ const NavBar = () => {
       <Link to="/fuelpricecalender">Fuel Price</Link>
     </li>
 
-    <li className="mb-4 bg-violet-200 border p-2 transition-transform transform hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
+    <li className="mb-4 bg-violet-300 border p-2 transition-transform transform hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
       <Link to="/getuser">Get User</Link>
     </li>
 
-    <li className="mb-4 bg-violet-100 border p-2 transition-transform transform hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
+    <li className="mb-4 bg-violet-300 border p-2 transition-transform transform hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
       <Link to="/updateuser">Update User</Link>
     </li>
   </ul>
