66408b69860b255fdbc9289783dcb243951f7801
ImrazAhmedTanim <tanimf1@gmail.com>
2024-02-08 14:49:36 +0600

    date

--- front/restapi/src/component/NavBar.js
+++ front/restapi/src/component/NavBar.js
@@ -15,7 +15,7 @@ const NavBar = () => {
   return (
 <nav  className="nav">
   <ul className="navbar flex flex-row justify-end ml-auto">
-    <li className="mb-4 bg-violet-300 border p-2 transition-transform transform hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
+    <li className="mb-4 bg-zinc-50 border p-2 transition-transform transform hover:bg-zinc-200 hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
       {isLoggedIn ? (
         <Link to="/" onClick={handleLogout}>Logout</Link>
       ) : (
@@ -23,22 +23,22 @@ const NavBar = () => {
       )}
     </li>
 
-    <li className="mb-4 bg-violet-300 border p-2 transition-transform transform hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
+    <li className="mb-4 bg-zinc-50 border p-2 transition-transform transform hover:bg-zinc-200 hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
       <Link to="/allusers">Get All Users</Link>
     </li>
 
-    <li className="mb-4 bg-violet-300 border p-2 transition-transform transform hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
+    <li className="mb-4 bg-zinc-50 border p-2 transition-transform transform hover:bg-zinc-200 hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
       <Link to="/fuelpricecalender">Fuel Price</Link>
     </li>
-    <li className="mb-4 bg-violet-300 border p-2 transition-transform transform hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
+    <li className="mb-4 bg-zinc-50 border p-2 transition-transform transform hover:bg-zinc-200 hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
       <Link to="/goldpricecalender">Gold Price</Link>
     </li>
 
-    <li className="mb-4 bg-violet-300 border p-2 transition-transform transform hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
+    <li className="mb-4 bg-zinc-50 border p-2 transition-transform transform hover:bg-zinc-200 hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
       <Link to="/getuser">Get User</Link>
     </li>
 
-    <li className="mb-4 bg-violet-300 border p-2 transition-transform transform hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
+    <li className="mb-4 bg-zinc-50 border p-2 transition-transform transform hover:bg-zinc-200 hover:scale-110 hover:border-t hover:border-r hover:border-b hover:border-l hover:border-black">
       <Link to="/updateuser">Update User</Link>
     </li>
   </ul>
